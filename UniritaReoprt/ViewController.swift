0c542b02f3bd3eb39e3bba61e450a74deb1268a1
RyotaShinzato <ryota3988@gmail.com>
2017-08-01 20:13:40 +0900

    Initial Commit

---
+++ UniritaReoprt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniritaReoprt
+//
+//  Created by snzt on 2017/08/01.
+//  Copyright © 2017年 snzt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
